56b87927a6d0e6c342a133960120c2ca922871ad
Josh <dev.joshm@gmail.com>
2026-07-14 20:38:56 -0400

    feat: list watermarks and filler presets over the api

---
+++ ErsatzTV.Core/Api/Filler/FillerPresetResponseModel.cs
@@ -0,0 +1,3 @@
+namespace ErsatzTV.Core.Api.Filler;
+
+public record FillerPresetResponseModel(int Id, string Name);
---
+++ ErsatzTV.Core/Api/Watermarks/WatermarkResponseModel.cs
@@ -0,0 +1,3 @@
+namespace ErsatzTV.Core.Api.Watermarks;
+
+public record WatermarkResponseModel(int Id, string Name);
